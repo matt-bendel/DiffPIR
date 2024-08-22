aa727be874c47b73357b424c502bbedf580f4991
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-08-22 17:39:27 -0400

    Noisy

--- main_ddpir_sisr.py
+++ main_ddpir_sisr.py
@@ -30,12 +30,12 @@ def main():
     # Preparation
     # ----------------------------------------
 
-    noise_level_img         = 0.0#12.75/255.0       # set AWGN noise level for LR image, default: 0
+    noise_level_img         = 12.75/255.0       # set AWGN noise level for LR image, default: 0
     noise_level_model       = noise_level_img   # set noise level of model, default: 0
     model_name              = 'diffusion_ffhq_10m'  # diffusion_ffhq_10m, 256x256_diffusion_uncond; set diffusino model
     testset_name            = 'ffhq_val_100'    # set testing set,  'imagenet_val' | 'ffhq_val'
     num_train_timesteps     = 1000
-    iter_num                = 20                # set number of sampling iterations
+    iter_num                = 100                # set number of sampling iterations
     iter_num_U              = 1                 # set number of inner iterations, default: 1
     skip                    = num_train_timesteps//iter_num     # skip interval
     sr_mode                 = 'cubic'            # 'blur', 'cubic' mode of sr up/down sampling

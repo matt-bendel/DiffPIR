46373e65e680c8ee40a9262a245db37e3ab5a463
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-11-06 14:36:22 -0500

    MNIST posterior fig

--- main_ddpir_inpainting.py
+++ main_ddpir_inpainting.py
@@ -328,9 +328,9 @@ def main():
 
                 # save the process
                 x_0 = (x/2+0.5)
-                if (i + 1) in [10, 35, 60]:
+                if (i + 1) in [10, 25, 50]:
                     import matplotlib.pyplot as plt
-                    plt.imsave(f'intermediate_diffpir_{idx}_{im_count}.png', clear_color(x0[0]))
+                    plt.imsave(f'intermediate_diffpir_{idx - 1}_{im_count}.png', clear_color(x0[0]))
                     im_count += 1
 
                 if save_progressive and (seq[i] in progress_seq):
